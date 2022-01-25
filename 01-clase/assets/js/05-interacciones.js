d637b0ba7b758af1b337beb74fb5ed4fb4ad4a97
abecerraguz <alejandro.becerra@publimetro.cl>
2022-01-24 21:53:17 -0300

    Apuntes de introduccion a JavaScript

--- 01-clase/assets/js/05-interacciones.js
+++ 01-clase/assets/js/05-interacciones.js
@@ -2,7 +2,7 @@ console.log('Interactuar con la consola')
 var numeroUno = parseInt(prompt('Ingrese un numero'))
 console.log('Tipo de dato--->', numeroUno)
 
-var numeroDos = parseInt( prompt('Ingrese un numero dos') )
+var numeroDos = parseInt( prompt('Ingrese un numero dos ') )
 alert(numeroDos)
 
 
